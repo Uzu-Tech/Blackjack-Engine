5ab6ae7fd882f97ec9041172d2e0bfe4336ac86b
Kobi Uzu <68594772+Uzu-Tech@users.noreply.github.com>
2025-02-04 11:46:29 +0000

    Refactor Hand and Player classes, add constants and lookups
    Updated Hand class:
    - Added BLACKJACK constant.
    - Made getHandValue method const.
    - Changed isSoftHand to a regular method.
    - Replaced is_hand_soft_ with num_aces_.
    
    Updated Player class:
    - Made several methods const: decideAction, displayAction, updateBankroll, displayHand.
    - Added constants for hand combinations.
    - Added strategy lookup tables.
    - Removed private strategy methods.
    - Added methods to generate lookup tables.

--- Blackjack Engine/hand.h
+++ Blackjack Engine/hand.h
@@ -8,6 +8,7 @@ public:
 	static constexpr int NUM_INITAL_CARDS = 2; // Initial number of cards for a hand
 	static constexpr int MAX_NUM_CARDS = 11; // 4 Aces, 4 Twos and 3 Threes = 21 
 	static constexpr int MAX_DECK_HAND = 17; // Dealer must hit on 16 or less
+	static constexpr int BLACKJACK = 21; // Blackjack value
 
 	// Contructors and Destructors
 	virtual ~Hand() {};
@@ -15,17 +16,18 @@ public:
 
 	// Deals two cards to the hand
 	void dealHand(Deck& deck);
-	// Returns the value of the hand
-	int getHandValue();
-	void clearHand();
 	// Adds a card to the hand
 	void hitHand(Deck& deck);
+	// Returns the value of the hand
+	int getHandValue() const;
+	void clearHand();
 	// Whether the hand has an Ace valued as 11
-	bool isSoftHand() const { return is_hand_soft_; };
+	bool isSoftHand() const;
+	bool isPairHand() const;
 
 protected:
 	std::vector<Card> hand_;
-	bool is_hand_soft_ = false; // Whether the hand has an Ace valued as 11
+	int num_aces_ = 0;
 };
 
 class Dealer : public Hand {
@@ -51,12 +53,12 @@ public:
 	// Calculates the bet based on the player's strategy
 	void calculateBet(int min_bet, int max_bet);
 	// Implements a strategy to decide the player's action
-	Player::Action decideAction(const Card& upcard);
+	Action decideAction(const Card& upcard) const;
 	// Displays the player's action to the console
-	void displayAction(Player::Action action);
+	void displayAction(Action action) const;
 	// Updates the player's bankroll based on the outcome
-	void updateBankroll(Player::Outcome outcome);
-	void displayHand();
+	void updateBankroll(Outcome outcome);
+	void displayHand() const;
 	void displayBetAndBankroll() const;
 	// Getters
 	double getBankroll() const { return bankroll_; };
@@ -67,8 +69,19 @@ private:
 	double bankroll_; // Player's available money
 	double bet_;
 
+	// Constants for the possible hand combinations
+	static constexpr int NUM_HARD_HANDS = 16; // 5-20 -> 16 cards
+	static constexpr int NUM_SOFT_HANDS = 8; // 13-20 -> 8 cards
+	static constexpr int NUM_PAIR_HANDS = 10; // 2-A -> 10 cards
+	static constexpr int NUM_DEALER_UPCARDS = 10; // 2-A -> 10 cards
+
+	// Strategy lookup tables
+	Action hard_hand_lookup[NUM_HARD_HANDS][NUM_DEALER_UPCARDS];
+	Action soft_hand_lookup[NUM_SOFT_HANDS][NUM_DEALER_UPCARDS];
+	Action pair_hand_lookup[NUM_PAIR_HANDS][NUM_DEALER_UPCARDS];
+
 	// Private strategy methods for the player
-	Player::Action normalHandStrategy_(const Card& upcard);
-	Player::Action softHandStrategy_(const Card& upcard);
-	Player::Action pairHandStrategy_(const Card& upcard);
+	Action generateHardHandLookupTable() const;
+	Action generateSoftHandLookupTable() const;
+	Action generatePairHandLookupTable() const;
 };
