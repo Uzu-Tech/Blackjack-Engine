7b545307400c01fb00ce1393c5125979ebf2dc39
Kobi Uzu <68594772+Uzu-Tech@users.noreply.github.com>
2025-02-07 08:16:02 +0000

    Implement Dealer class with core functionalities
    Added dealer.h and dealer.cpp to define and implement the Dealer class. The Dealer class includes:
    - An enum class Action for HIT and STAND actions.
    - Methods to deal and hit using a Deck object.
    - A debugging method setHand to manually set the dealer's hand.
    - A getter method getUpcard with error handling for an empty hand.
    - A method decideAction to determine the dealer's action based on hand value.
    - Methods displayHand and displayAction to show the dealer's hand and action.

---
+++ Blackjack Engine/dealer.cpp
@@ -0,0 +1,46 @@
+#include "dealer.h"
+
+// Dealer Methods
+Dealer::Action Dealer::decideAction()
+{
+	int hand_value = hand_.getHandValue();
+	return (
+		(hand_value < HandConstants::MAX_DECK_HAND || (hand_.isSoftHand() && hand_value == HandConstants::MAX_DECK_HAND)) ?
+		Dealer::Action::HIT : Dealer::Action::STAND
+	);
+}
+
+void Dealer::displayHand(bool hidden) const
+{
+	if (hand_.isEmpty()) {
+		throw std::runtime_error("Cannot display empty hand");
+	}
+
+	std::cout << "Dealer Hand: ";
+
+	if (!hidden) {
+		// Display all cards
+		for (const Card& card : hand_.getHand()) {
+			std::cout << card.getRankAsString() << " ";
+		}
+		std::cout << "\nHand Value: " << hand_.handValueToString();
+	}
+	else {
+		// Display only the first card and hide the second
+		std::cout << hand_.getHand()[0].getRankAsString() << " ";
+		std::cout << "??";
+	}
+	std::cout << "\n\n";
+}
+
+void Dealer::displayAction(Dealer::Action action) const 
+{
+	switch (action) {
+	case Dealer::Action::HIT:
+		std::cout << "Dealer hits\n\n";
+		break;
+	case Dealer::Action::STAND:
+		std::cout << "Dealer stands\n\n";
+		break;
+	}
+}
---
+++ Blackjack Engine/dealer.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "deck.h"
+#include "hand.h"
+
+class Dealer {
+public:
+	// Possible actions for the dealer
+	enum class Action { HIT, STAND };
+
+	// Hand methods
+	void dealHand(Deck& deck) { hand_.dealHand(deck); };
+	void hitHand(Deck& deck) { hand_.hitHand(deck); };
+
+	// Debugging method to set the dealer's hand
+	void setHand(const Card& card1, const Card& card2) {
+		hand_.addCard(card1);
+		hand_.addCard(card2);
+	}
+
+	// Getters
+	const Card& getUpcard() const { 
+		if (hand_.isEmpty()) {
+			throw std::runtime_error("Cannot get upcard from empty hand");
+		}
+
+		return hand_.getHand().front(); 
+	};
+
+	// Auto plays the dealer's hand
+	Action decideAction();
+	void displayHand(bool hidden = true) const;
+	void displayAction(Dealer::Action action) const;
+
+private:
+	Hand hand_ = Hand();
+};
+
